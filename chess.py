907beea5f0ac1023b2bfab11cf98b2d7e58d21ed
7038165 <jacob.queen@mines.sdsmt.edu>
2017-12-06 22:42:57 -0700

    Initialized file.

---
+++ chess.py
@@ -0,0 +1,15 @@
+#This will be a fully functional chess game.
+#It will allow for local play against another
+#human or AI player.
+#It will also allow direct connection play.
+#Finally it will be displayed in GUI format.
+import tkinter
+
+
+def main():
+   #stuff goes here 
+   test = "test"
+
+
+if __name__ == "__main__":
+    main()
